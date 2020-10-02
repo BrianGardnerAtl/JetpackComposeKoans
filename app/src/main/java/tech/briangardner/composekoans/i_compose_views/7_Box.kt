f16544fe3600983c288095c996a823e964968edf
Brian Gardner <bgardner@bignerdranch.com>
2020-10-02 14:59:06 -0400

    Add box and stack todos

--- app/src/main/java/tech/briangardner/composekoans/i_compose_views/7_Box.kt
+++ app/src/main/java/tech/briangardner/composekoans/i_compose_views/7_Box.kt
@@ -1,17 +1,117 @@
 package tech.briangardner.composekoans.i_compose_views
 
+import androidx.compose.foundation.Box
 import androidx.compose.runtime.Composable
+import androidx.ui.tooling.preview.Preview
 
 object BoxKoans {
-    // TODO: Create function for each task
-    //  Create a description function to explain what the function should do
-    //  Create preview functions to display the output of each function
-    val tasks = """
 
-    """.trimIndent()
+    fun todoTask0(): Nothing = TODO(
+        """
+        Task 0.
+        Add a box with an icon button child. The icon button should display the heart icon
+        """,
+    )
 
     @Composable
     fun task0() {
-
+        todoTask0()
     }
+
+    fun todoTask1(): Nothing = TODO(
+        """
+        Task 1.
+        Make the box have a circle shape
+        """,
+    )
+
+    @Composable
+    fun task1() {
+        todoTask1()
+    }
+
+    fun todoTask2(): Nothing = TODO(
+        """
+        Task 2.
+        Set a blue background color on a round box
+        """,
+    )
+
+    @Composable
+    fun task2() {
+        todoTask2()
+    }
+
+    fun todoTask3(): Nothing = TODO(
+        """
+        Task 3.
+        Set the box border
+        """,
+    )
+
+    @Composable
+    fun task3() {
+        todoTask3()
+    }
+
+    fun todoTask4(): Nothing = TODO(
+        """
+        Task 4.
+        Set an 8 dp padding
+        """,
+    )
+
+    @Composable
+    fun task4() {
+        todoTask4()
+    }
+
+    fun todoTask5(): Nothing = TODO(
+        """
+        Task 5.
+        Set the gravity of the box to bottom center
+        """,
+    )
+
+    @Composable
+    fun task5() {
+        todoTask5()
+    }
+}
+
+// Preview functions
+@Preview
+@Composable
+fun previewBoxTask0() {
+    BoxKoans.task0()
+}
+
+@Preview
+@Composable
+fun previewBoxTask1() {
+    BoxKoans.task1()
+}
+
+@Preview
+@Composable
+fun previewBoxTask2() {
+    BoxKoans.task2()
+}
+
+@Preview
+@Composable
+fun previewBoxTask3() {
+    BoxKoans.task3()
+}
+
+@Preview
+@Composable
+fun previewBoxTask4() {
+    BoxKoans.task4()
+}
+
+@Preview
+@Composable
+fun previewBoxTask5() {
+    BoxKoans.task5()
 }
--- app/src/main/java/tech/briangardner/composekoans/i_compose_views/8_Stack.kt
+++ app/src/main/java/tech/briangardner/composekoans/i_compose_views/8_Stack.kt
@@ -1,18 +1,48 @@
 package tech.briangardner.composekoans.i_compose_views
 
 import androidx.compose.runtime.Composable
+import androidx.ui.tooling.preview.Preview
 
 object StackKoans {
-    // TODO: Create function for each task
-    //  Create a description function to explain what the function should do
-    //  Create preview functions to display the output of each function
-    val tasks = """
-        0 -> Create a Stack with 3 Box children
-        1 -> Set child gravity to move them around
-    """.trimIndent()
+
+    fun todoTask0(): Nothing = TODO(
+        """
+        Task 0.
+        Create a task with 3 box children.
+        The first box should be red, the second should be green, the third should be blue
+        """,
+    )
 
     @Composable
     fun task0() {
-
+        todoTask0()
     }
+
+    fun todoTask1(): Nothing = TODO(
+        """
+        Task 1.
+        Set the child gravity of the three boxes.
+        The first should be top start
+        The second should be center
+        The third should be bottom end
+        """,
+    )
+
+    @Composable
+    fun task1() {
+        todoTask1()
+    }
+}
+
+// Preview functions
+@Preview
+@Composable
+fun previewStackTask0() {
+    StackKoans.task0()
+}
+
+@Preview
+@Composable
+fun previewStackTask1() {
+    StackKoans.task1()
 }
