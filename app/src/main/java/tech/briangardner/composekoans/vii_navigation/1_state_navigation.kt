3eed6e133c93d4a0631185be5c293a796d0f9638
Brian Gardner <bgardner@bignerdranch.com>
2020-10-08 14:52:27 -0400

    Remove navigation for now

--- app/src/main/java/tech/briangardner/composekoans/vii_navigation/1_state_navigation.kt
+++
@@ -1,5 +0,0 @@
-package tech.briangardner.composekoans.vii_navigation
-
-object StateNavigationKoans {
-
-}
--- app/src/main/java/tech/briangardner/composekoans/vii_navigation/StateNavigationActivity.kt
+++
@@ -1,7 +0,0 @@
-package tech.briangardner.composekoans.vii_navigation
-
-import androidx.appcompat.app.AppCompatActivity
-
-class StateNavigationActivity : AppCompatActivity() {
-
-}
